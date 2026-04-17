3c96baf3b1f4284909994ba2944f46adc0630a34
sateesh-1010 <sathikumar12950@gmail.com>
2026-04-17 10:52:14 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,55 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module mealy_101 (
+    input clk,
+    input reset,
+    input x,
+    output reg y
 );
+reg [1:0] state;
+parameter S0 = 2'b00;
+parameter S1 = 2'b01;
+parameter S2 = 2'b10;
+// mealy
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        state <= S0;
+        y <= 0;
+    end
+    else begin
+        case (state)
+            S0: begin
+                if (x == 1) begin
+                    state <= S1;
+                    y <= 0;
+                end else begin
+                    state <= S0;
+                    y <= 0;
+                end
+            end
+            S1: begin
+                if (x == 0) begin
+                    state <= S2;
+                    y <= 0;
+                end else begin
+                    state <= S1;
+                    y <= 0;
+                end
+            end
+            S2: begin
+                if (x == 1) begin
+                    state <= S1;
+                    y <= 1;   
+                end else begin
+                    state <= S0;
+                    y <= 0;
+                end
+            end
+        default: begin
+                state <= S0;
+                y <= 0;
+            end
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+        endcase
+    end
+end
 
 endmodule
