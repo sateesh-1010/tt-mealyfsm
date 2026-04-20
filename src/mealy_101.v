949229f9ac0c1aa0eda611c9957bf135d682a5cc
sateesh-1010 <sathikumar12950@gmail.com>
2026-04-20 10:27:47 +0530

    Create mealy_101.v

---
+++ src/mealy_101.v
@@ -0,0 +1,43 @@
+module mealy_101(
+    input clk,
+    input reset,
+    input x,
+    output reg y
+);
+
+    reg [1:0] state;
+
+    parameter S0 = 2'b00;
+    parameter S1 = 2'b01;
+    parameter S2 = 2'b10;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            state <= S0;
+            y <= 0;
+        end else begin
+            case (state)
+                S0: begin
+                    if (x == 1) state <= S1;
+                    else state <= S0;
+                    y <= 0;
+                end
+                S1: begin
+                    if (x == 0) state <= S2;
+                    else state <= S1;
+                    y <= 0;
+                end
+                S2: begin
+                    if (x == 1) begin
+                        state <= S1;
+                        y <= 1;
+                    end else begin
+                        state <= S0;
+                        y <= 0;
+                    end
+                end
+            endcase
+        end
+    end
+
+endmodule
